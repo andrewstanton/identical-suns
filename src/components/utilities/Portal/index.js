4124d17cb15a76cb4691dbda2448df3245ee5227
Andrew Stantron <andrew@toddprod.com>
2019-10-02 11:06:14 -0400

    Fixed for gatsby

--- src/components/utilities/Portal/index.js
+++ src/components/utilities/Portal/index.js
@@ -1,26 +1,29 @@
-import { Component } from "react";
-import ReactDOM from "react-dom";
+import { Component } from "react"
+import ReactDOM from "react-dom"
 
-const portalRoot = document.getElementById("portal");
+const portalRoot = document.getElementById("portal")
 
 class Portal extends Component {
   constructor() {
-    super();
-    this.el = document.createElement("div");
+    super()
+    this.el = null
+    if (typeof document !== "undefined") {
+      this.el = document.createElement("div")
+    }
   }
 
   componentDidMount() {
-    portalRoot.appendChild(this.el);
+    portalRoot.appendChild(this.el)
   }
 
   componentWillUnmount() {
-    portalRoot.removeChild(this.el);
+    portalRoot.removeChild(this.el)
   }
 
   render() {
-    const { children } = this.props;
-    return ReactDOM.createPortal(children, this.el);
+    const { children } = this.props
+    return ReactDOM.createPortal(children, this.el)
   }
 }
 
-export default Portal;
+export default Portal
